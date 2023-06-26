c9378d1aa866e71ed2c9e6ca0f2d74e20cb4c290
Taylan ALTUN <taylanaltun@gmail.com>
2023-06-26 19:49:21 +0300

    Implement NotFoundException

--- NLayer.API/Middlewares/UseCustomExceptionHandler.cs
+++ NLayer.API/Middlewares/UseCustomExceptionHandler.cs
@@ -1,6 +1,38 @@
-﻿namespace NLayer.API.Middlewares
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using NLayer.Core.DTOs;
+using NLayer.Service.Exceptions;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace NLayer.API.Middlewares
 {
-    public class Class
+    public static class UseCustomExceptionHandler
     {
+        public static void UseCustomException(this IApplicationBuilder app) 
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    var statusCode = exceptionFeature.Error switch
+                    {
+                        ClientSideException => 400,
+                        NotFoundException => 404,
+                        _ => 500
+                    };
+
+                    context.Response.StatusCode = statusCode;
+
+                    var response = CustomResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                });
+            });
+        }
     }
 }
--- NLayer.Service/Services/Service.cs
+++ NLayer.Service/Services/Service.cs
@@ -2,6 +2,7 @@
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
+using NLayer.Service.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -48,6 +49,11 @@ namespace NLayer.Service.Services
 
         public async Task<T> GetByIdAsync(int id)
         {
+            var hasProduct = await _repository.GetByIdAsync(id);
+
+            if(hasProduct == null)
+                throw new NotFoundException($"{typeof(T).Name} ({id}) not found");
+
             return await _repository.GetByIdAsync(id);  
         }
 
